a119f563f5d411fd4327e1671fa22178251eb515
Tron <67748834+moneshkovi@users.noreply.github.com>
2025-04-28 07:51:40 +0000

    lab 6

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Card = ({description, alt_description, id, user, urls, likes}) => {
+ 
 
   const style = {
     backgroundImage: `url(${urls.small})`
